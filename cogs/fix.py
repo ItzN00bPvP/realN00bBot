7d9f1d689b786dfa6b958e99273f177d3bf32509
Philipp_DE <philipphaluschak@gmail.com>
2021-10-07 18:58:54 +0200

    feat: fix

---
+++ cogs/fix.py
@@ -0,0 +1,13 @@
+from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
+
+class Fix(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @cog_ext.cog_slash(name="fix", description="If you have a problem this command helps you to fix it.")
+    async def _fix(self, ctx: SlashContext):
+        await ctx.send("<@257642666244833281> go do fix!")
+
+def setup(bot):
+    bot.add_cog(Fix(bot))
