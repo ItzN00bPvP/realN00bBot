d8a163ad6dc2d5929d5019010dde6e5461077330
Philipp_DE <philipphaluschak@gmail.com>
2021-10-21 22:26:42 +0200

    feat: shorturl stats

--- cogs/shorturl.py
+++ cogs/shorturl.py
@@ -26,14 +26,41 @@ class shorturl(commands.Cog):
         )
     ])
     @commands.has_any_role(*roleslevel.ppa)
-    async def _grant(self, ctx: SlashContext, url: str, tag: str):
+    async def _create(self, ctx: SlashContext, url: str, tag: str):
         try:
             surl = self.yourls.shorten(url, keyword=tag)
             print(surl)
-            await ctx.send(
-                "URL got shortened:\nLong URL:```" + surl['url']['url'] + "```Short URL:```" + surl['shorturl'] + "```")
+            await ctx.send(f"URL got shortened:\n"
+                           f"Long URL:```{surl['url']['url']}```"
+                           f"Short URL:```{surl['shorturl']}```")
         except pyourls3.exceptions.Pyourls3APIError as e:
             await ctx.send(str(e))
+
+    @cog_ext.cog_subcommand(guild_ids=config.slash_shorturl_create, base="shorturl", name="stats", options=[
+        create_option(
+            name="tag",
+            description="The TAG you want to get the stats for mcahto.me/<TAG> or all.",
+            option_type=3,
+            required=True
+        )
+    ])
+    @commands.has_any_role(*roleslevel.member)
+    async def _stats(self, ctx: SlashContext, tag: str):
+        if tag == "all":
+            urls = self.yourls.stats()
+            await ctx.send(f"General link statistics:\n"
+                           f"Total Links: ```{urls['total_links']}```"
+                           f"Total Clicks: ```{urls['total_clicks']}```")
+
+            return
+        try:
+            urls = self.yourls.url_stats(tag)
+            await ctx.send(f"Stats for shortened URL: ```{urls['shorturl']}```"
+                           f"Long URL: ```{urls['url']}```"
+                           f"Clicks: ```{urls['clicks']}```"
+                           f"Created: ```{urls['timestamp']}```")
+        except:
+            await ctx.send(f"The TAG probably doesn't exist! `{tag}`", delete_after=10)
 
 
 def setup(bot):
