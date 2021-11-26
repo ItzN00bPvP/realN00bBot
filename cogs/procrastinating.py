385cd1e91ce35078c2b38038629590e8b8d23efb
Philipp_DE <philipphaluschak@gmail.com>
2021-11-26 18:29:00 +0100

    feat: procrastinating with verbs

--- cogs/procrastinating.py
+++ cogs/procrastinating.py
@@ -4,7 +4,7 @@ import re
 import requests
 from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
-from discord_slash.utils.manage_commands import create_option
+from discord_slash.utils.manage_commands import create_option, create_choice
 
 from config import config
 from config.config import cloudflare_header, cloudflare_procrastinatingathomezoneid
@@ -20,18 +20,37 @@ class procrastinating(commands.Cog):
             description="The user who is procrastinating.",
             option_type=3,
             required=True,
+        ), create_option(
+            name="verb",
+            description="is / are / am",
+            option_type=3,
+            required=False,
+            choices=[
+                create_choice(
+                    name="is",
+                    value="is"
+                ),
+                create_choice(
+                    name="are",
+                    value="are"
+                ),
+                create_choice(
+                    name="am",
+                    value="am"
+                )
+            ]
         )])
-    async def _bald(self, ctx: SlashContext, name: str):
-        if not re.compile("^[A-Za-z0-9](?:[A-Za-z0-9\-]{0,61}[A-Za-z0-9])?$").match(name):
+    async def _bald(self, ctx: SlashContext, name: str, verb: str):
+        if not re.compile("^[A-Za-z0-9](?:[A-Za-z0-9\-]{0,58}[A-Za-z0-9])?$").match(name):
             await ctx.send("Invalid Name!")
             return
 
-        body = {"type": "CNAME", "name": name, "content": "proxy.mcatho.me", "ttl": 1, "proxied": True}
+        body = {"type": "CNAME", "name": f"{name}-{verb}", "content": "proxy.mcatho.me", "ttl": 1, "proxied": True}
         req = requests.post(f"https://api.cloudflare.com/client/v4/zones/{cloudflare_procrastinatingathomezoneid}/dns_records",
                             headers=cloudflare_header, data=json.JSONEncoder().encode(body))
 
-        if req.json()['result'] is None and  req.json()['errors'][0]['code'] == 81053:
-            await ctx.send(content=f"https://{name}.procrastinatingathome.com/")
+        if req.json()['result'] is None and req.json()['errors'][0]['code'] == 81053:
+            await ctx.send(content=f"https://{name}-{verb}.procrastinatingathome.com/")
             return
 
         m = await ctx.send("3")
@@ -40,7 +59,7 @@ class procrastinating(commands.Cog):
         time.sleep(1)
         await m.edit(content=f"1")
         time.sleep(1)
-        await m.edit(content=f"https://{name}.procrastinatingathome.com/")
+        await m.edit(content=f"https://{name}-{verb}.procrastinatingathome.com/")
 
 
 
