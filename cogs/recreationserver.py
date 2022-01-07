fe06328c444c7d5e99df5ee2443f1619a6782c07
Philipp_DE <philipphaluschak@gmail.com>
2022-01-07 19:27:35 +0100

    feat: recreationserver create

---
+++ cogs/recreationserver.py
@@ -0,0 +1,39 @@
+import re
+import requests
+
+from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
+from discord_slash.utils.manage_commands import create_option
+
+from config.config import roleslevel
+from config import config
+
+
+class recreationserver(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @cog_ext.cog_subcommand(guild_ids=config.slash_mcperms_grant, base="recreationserver", name="create", options=[
+        create_option(
+            name="project",
+            description="The Name of the project / server.",
+            option_type=3,
+            required=True
+        )
+    ])
+    @commands.has_any_role(*roleslevel.ppa)
+    async def _create(self, ctx: SlashContext, project: str):
+        if not re.compile("^[A-Za-z0-9](?:[A-Za-z0-9\-]{0,61}[A-Za-z0-9])?$").match(project):
+            await ctx.send("Invalid Name!")
+            return
+        req = requests.request("POST", f"https://api.mcathome.dev/recreationserver/create/{project}/", headers={'auth': config.apimcathomedev_authtoken})
+        if req.status_code != 200:
+            await ctx.send(f"Something went wrong status: {req.status_code}")
+        respjson = req.json()
+        address = respjson["address"]
+        await ctx.send(f"Server {project} created.\nProject:```{address}```")
+
+
+
+def setup(bot):
+    bot.add_cog(recreationserver(bot))
--- utils/mcpermsapi.py
+++ utils/mcpermsapi.py
@@ -5,21 +5,23 @@ header = {'auth': mcperms_authtoken}
 
 
 def grantperms(server: str, perms: str, mcname: str, discordid: str):
-    req = requests.post(f"https://mcperms.api.mcatho.me/grant/{server}/{perms}/{discordid}/{mcname}", headers=header)
+    req = requests.post(f"https://api.mcathome.dev/mcperms/grant/{server}/{perms}/{discordid}/{mcname}", headers=header)
     if req.status_code == 200:
         return True, req.json()
     return False, f"Something went wrong status: {req.status_code}"
 
 
 def revokeperms(server: str, user: str):
-    req = requests.post(f"https://mcperms.api.mcatho.me/revoke/{server}/{user}", headers=header)
+    req = requests.post(f"https://api.mcathome.dev/mcperms/revoke/{server}/{user}", headers=header)
     if req.status_code == 200:
         return True, req.json()
     return False, f"Something went wrong status: {req.status_code}"
 
 
 def getglobalperms():
-    req = requests.get(f"https://mcperms.api.mcatho.me/globalperms", headers=header)
+    req = requests.get(f"https://api.mcathome.dev/mcperms/globalperms", headers=header)
     if req.status_code == 200:
         return True, req.json()
     return False, f"Something went wrong status: {req.status_code}"
+
+
