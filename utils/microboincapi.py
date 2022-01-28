31e71563917b0c9c70f308a0a269f74bb01a27b4
Philipp_DE <philipphaluschak@gmail.com>
2022-01-28 18:24:59 +0100

    fix: sort leaderboard by good tasks

--- utils/microboincapi.py
+++ utils/microboincapi.py
@@ -8,9 +8,8 @@ header = {'Authorization': microboincapikey, 'user-agent': 'realN00bBot/0.0.1'}
 
 def getleaderboardbyid(appid: int):
     r = requests.get(url=f"{microboincapi_endpoint}/leaderboards/byproject/{appid}", headers=header)
-    print(r.status_code)
     if r.status_code == 200:
-        return True, json.loads(str(r.content)[2:-1].replace("\\n", "\n"))
+        return True, r.json()
     elif r.status_code == 404:
         return False, "App not found!"
     return False, "<@374245848659263488> something went wrong code: " + str(r.status_code)
