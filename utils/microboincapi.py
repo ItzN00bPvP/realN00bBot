3d1f9b41df2723e85f512048eedde7915f8ee05a
ItzN00bPvP <philipphaluschak@gmail.com>
2022-08-19 04:51:28 +0200

    fix: result

--- utils/microboincapi.py
+++ utils/microboincapi.py
@@ -60,7 +60,7 @@ def getresultsbyappid(appid: int):
     r = requests.get(url=f"{microboincapi_endpoint}/results/byprojectid/{appid}", headers=header)
 
     if r.status_code == 200:
-        return True, r.json()
+        return True, r.text
     elif r.status_code == 404:
         return False, "App not found!"
     return False, "<@374245848659263488> something went wrong code: " + str(r.status_code)
